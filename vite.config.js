022ff3247ca32623e41c70b4304c8f66e97e8137
Ayesha Lomaski da Silveira Costa <lomaski@users.noreply.github.com>
2026-09-04 17:47:34 -0300

    Update base path in vite.config.js
    Set the base path for the Vite project.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/devmovie/', // Substitua exatamente pelo nome do seu repositório se for diferente de 'devmovie'
 })
