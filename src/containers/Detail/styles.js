721b7e61a4a5a7088c41b5771029850c43c2ec56
Ayesha Lomaski da Silveira Costa <lomaski@users.noreply.github.com>
2026-09-22 11:19:14 -0300

    modified:   src/containers/Detail/styles.js

--- src/containers/Detail/styles.js
+++ src/containers/Detail/styles.js
@@ -69,6 +69,8 @@ export const Container = styled.div`
     align-items: center;
     margin-top: -50px; /* Sobe um pouco menos no celular */
     gap: 20px;
+    padding: 20px;
+    text-align: center;
   }
 `;
 
